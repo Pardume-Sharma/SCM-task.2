cd4073cba11869f64e96be4697fd6eb403f20409
Pardume_Sharma <linkinpardume07@gmail.com>
2023-02-22 13:08:51 +0530

    Basic skeleton of the program made..!

---
+++ Tasks.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int number,choice;
+    printf("Enter number:\n");
+    scanf("%d",&number);
+    printf("Enter choice:\n\t1 to check even/odd\n\t2 to check prime\n\t3 to print all numbers till n\n\t4 to print table\n\t5 to print all even and odd numbers separatly\n\t6 to print sum of all numbers till n\n\t7 to print all prime numbers till n\n\t8 to print fibonacci/tribonacci series till n separtely\n\t9 to print all composite no. till n\n\n>>");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        default:
+            printf("Invalid choice");
+        return 0;
+    }
+}
